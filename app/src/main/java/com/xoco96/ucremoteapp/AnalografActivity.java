548fc8ae773575046e020976e4162af38366c34f
XOCO96 <lalo_1010_@hotmail.com>
2020-07-13 14:57:54 -0500

    Nuevo activity para el dibujado de la gráfica

---
+++ app/src/main/java/com/xoco96/ucremoteapp/AnalografActivity.java
@@ -0,0 +1,14 @@
+package com.xoco96.ucremoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnalografActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analograf);
+    }
+}
