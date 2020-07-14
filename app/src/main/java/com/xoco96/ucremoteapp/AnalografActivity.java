d149d62ddcc112db97155ce64db29e442fbeb6cf
XOCO96 <lalo_1010_@hotmail.com>
2020-07-14 12:26:26 -0500

    Gráfica de ejemplo para verificar la funcionalidad

--- app/src/main/java/com/xoco96/ucremoteapp/AnalografActivity.java
+++ app/src/main/java/com/xoco96/ucremoteapp/AnalografActivity.java
@@ -6,12 +6,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+import android.graphics.Color;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnalografActivity extends AppCompatActivity {
+
+
+    LineChartView lineChartView;
+    String[] axisData = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
+            "j", "k", "l"};
+    int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analograf);
+
+        lineChartView = findViewById(R.id.chart);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Sales in millions");
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
     }
 
     public void onClickBack(View view) {
